23ca74dfb599048f2b460e36a8b66ff344bc9fff
Alex-Reitz <alex.j.reitz@gmail.com>
2020-03-30 16:47:56 -0400

    refactor

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+modules.exports = {
+  apiKey: "AIzaSyC-VrHapS8dTe0w_xQQ3m8tD7TC1GwkHXs",
+  authDomain: "socialape-8d830.firebaseapp.com",
+  databaseURL: "https://socialape-8d830.firebaseio.com",
+  projectId: "socialape-8d830",
+  storageBucket: "socialape-8d830.appspot.com",
+  messagingSenderId: "361999820063",
+  appId: "1:361999820063:web:e8d342f531c5e5c440bc2a",
+  measurementId: "G-MBC9C14DPW"
+};
